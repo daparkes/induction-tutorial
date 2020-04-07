eaef8e432f57d9f5dcc8f6e4d746da3d9beb472c
Dan Parkes <daniel.parkes@digital.hmrc.gov.uk>
2020-04-07 13:37:22 +0100

    added method bodies for tests

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc.{BaseController, ControllerComponents}
+
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def index = TODO
+
+  def create = TODO
+
+  def read(id: String) = TODO
+
+  def update(id: String) = TODO
+
+  def delete(id: String) = TODO
+}
